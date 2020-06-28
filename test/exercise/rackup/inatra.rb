3b0a4b902d2591b8ba05a91ca1a2e3885d657780
KiryhaPikoff <pikov_kirya@mail.ru>
2020-06-28 16:36:25 +0400

    Inatra completed.

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,7 +1,28 @@
 module Inatra
   class << self
-    def routes(&block); end
+    def routes(&block)
+      @routes = {}
+      instance_eval(&block)
+    end
 
-    def call(env); end
+    def call(env)
+      method = env['REQUEST_METHOD'].to_s.downcase
+      path   = env['PATH_INFO'].to_s.downcase
+
+      @routes[method][path].nil? ? [404, {}, 'Not Found'] : @routes[method][path].call
+    end
+
+    def method_missing(method, path, &block)
+      method = method.to_s.downcase
+      path   = path.to_s.downcase
+      @routes[method] ||= @routes.store(method, {})
+      @routes[method].store(path, block)
+    end
+
+    def respond_to_missing?(method, path)
+      method = method.to_s.downcase
+      path   = path.to_s.downcase
+      @routes.include?(method) && @routes[method].include?(path) || super
+    end
   end
 end
